f243fc64c7cce4fdfcb3688a185c7ce296065cf2
Ark <189933909+ark1368@users.noreply.github.com>
2025-03-11 22:57:31 -0700

    Fix UI Color (#9)
    Signed-off-by: ark1368 <189933909+ark1368@users.noreply.github.com>

--- Content.Client/Administration/UI/CustomControls/PlayerListControl.xaml.cs
+++ Content.Client/Administration/UI/CustomControls/PlayerListControl.xaml.cs
@@ -44,7 +44,7 @@ public sealed partial class PlayerListControl : BoxContainer
         PopulateList(_adminSystem.PlayerList);
         FilterLineEdit.OnTextChanged += _ => FilterList();
         _adminSystem.PlayerListChanged += PopulateList;
-        BackgroundPanel.PanelOverride = new StyleBoxFlat { BackgroundColor = new Color(32, 32, 40) };
+        BackgroundPanel.PanelOverride = new StyleBoxFlat { BackgroundColor = new Color(54, 54, 54) };
     }
 
     public IReadOnlyList<PlayerInfo> PlayerInfo => _playerList;
