71c2496b7e2f92d9f9d7de4b3ce5afd3ce866b9d
Ark <189933909+ark1368@users.noreply.github.com>
2025-03-11 21:04:07 -0700

    Shuttle Impacts (#6)
    * Ship Impacts II (#109)
    
    (cherry picked from commit 7336eb3ce8a2bbb0ba244f40a4898cc5bb8b07a8)
    
    * Magboots Shuttle Impact Hotfix (#119)
    
    Signed-off-by: ark1368 <189933909+ark1368@users.noreply.github.com>
    
    (cherry picked from commit 8cd77c7e3fc4f154040bfe8b356ec790338e68cb)
    Signed-off-by: ark1368 <189933909+ark1368@users.noreply.github.com>
    
    * Ship Tile Breakage Hotfix (#116)
    
    Signed-off-by: ark1368 <189933909+ark1368@users.noreply.github.com>
    (cherry picked from commit 10c1d6dc9da7da3b71b0156f49fd277791408fb7)
    
    ---------
    
    Signed-off-by: ark1368 <189933909+ark1368@users.noreply.github.com>

--- Content.Server/Shuttles/Systems/ShuttleSystem.Impact.cs
+++ Content.Server/Shuttles/Systems/ShuttleSystem.Impact.cs
@@ -7,6 +7,14 @@ using Robust.Shared.Map;
 using Robust.Shared.Physics.Events;
 using Robust.Shared.Map.Components;
 using Content.Shared.Damage;
+using Content.Shared.Buckle.Components;
+using Content.Shared.Mobs.Components;
+using Content.Shared.Slippery;
+using Content.Shared.Inventory;
+using Content.Shared.Clothing;
+using Content.Shared.Item;
+using Content.Shared.Item.ItemToggle;
+using Content.Shared.Item.ItemToggle.Components;
 
 namespace Content.Server.Shuttles.Systems;
 
@@ -14,6 +22,8 @@ public sealed partial class ShuttleSystem
 {
     [Dependency] private readonly MapSystem _mapSys = default!;
     [Dependency] private readonly DamageableSystem _damageSys = default!;
+    [Dependency] private readonly InventorySystem _inventorySystem = default!;
+    [Dependency] private readonly ItemToggleSystem _toggle = default!;
 
     /// <summary>
     /// Minimum velocity difference between 2 bodies for a shuttle "impact" to occur.
@@ -66,13 +76,83 @@ public sealed partial class ShuttleSystem
 
         var energy = ourBody.Mass * Math.Pow(jungleDiff, 2) / 2;
         var dir = (ourVelocity.Length() > otherVelocity.Length() ? ourVelocity : -otherVelocity).Normalized();
-        ProcessTile(uid, ourGrid, (Vector2i) ourPoint, (float) energy, -dir);
-        ProcessTile(args.OtherEntity, otherGrid, (Vector2i) otherPoint, (float) energy, dir);
+        
+        // Convert the collision point directly to tile indices without creating intermediate EntityCoordinates
+        // ourPoint and otherPoint are already in local space of their respective entities
+        var ourTile = new Vector2i((int)Math.Floor(ourPoint.X / ourGrid.TileSize), (int)Math.Floor(ourPoint.Y / ourGrid.TileSize));
+        var otherTile = new Vector2i((int)Math.Floor(otherPoint.X / otherGrid.TileSize), (int)Math.Floor(otherPoint.Y / otherGrid.TileSize));
+        
+        ProcessTile(uid, ourGrid, ourTile, (float) energy, -dir);
+        ProcessTile(args.OtherEntity, otherGrid, otherTile, (float) energy, dir);
 
         var coordinates = new EntityCoordinates(ourXform.MapUid.Value, args.WorldPoint);
         var volume = MathF.Min(10f, 1f * MathF.Pow(jungleDiff, 0.5f) - 5f);
         var audioParams = AudioParams.Default.WithVariation(SharedContentAudioSystem.DefaultVariation).WithVolume(volume);
         _audio.PlayPvs(_shuttleImpactSound, coordinates, audioParams);
+
+        // Knockdown unbuckled entities on both grids
+        KnockdownEntitiesOnGrid(uid);
+        KnockdownEntitiesOnGrid(args.OtherEntity);
+    }
+
+    /// <summary>
+    /// Knocks down all unbuckled entities on the specified grid.
+    /// </summary>
+    private void KnockdownEntitiesOnGrid(EntityUid gridUid)
+    {
+        if (!TryComp<MapGridComponent>(gridUid, out var grid))
+            return;
+
+        // Find all entities on the grid
+        var buckleQuery = GetEntityQuery<BuckleComponent>();
+        var noSlipQuery = GetEntityQuery<NoSlipComponent>();
+        var magbootsQuery = GetEntityQuery<MagbootsComponent>();
+        var itemToggleQuery = GetEntityQuery<ItemToggleComponent>();
+        var knockdownTime = TimeSpan.FromSeconds(5);
+
+        // Get all entities with MobState component on the grid
+        var query = EntityQueryEnumerator<MobStateComponent, TransformComponent>();
+
+        while (query.MoveNext(out var uid, out var mobState, out var xform))
+        {
+            // Skip entities not on this grid
+            if (xform.GridUid != gridUid)
+                continue;
+
+            // If entity has a buckle component and is buckled, skip it
+            if (buckleQuery.TryGetComponent(uid, out var buckle) && buckle.Buckled)
+                continue;
+
+            // Skip if the entity directly has NoSlip component
+            if (noSlipQuery.HasComponent(uid))
+                continue;
+
+            // Check if the entity is wearing magboots
+            bool hasMagboots = false;
+
+            // Check if they're wearing shoes with NoSlip component or activated magboots
+            if (_inventorySystem.TryGetSlotEntity(uid, "shoes", out var shoes))
+            {
+                // If shoes have NoSlip component
+                if (noSlipQuery.HasComponent(shoes))
+                {
+                    hasMagboots = true;
+                }
+                // If shoes are magboots and they are activated
+                else if (magbootsQuery.HasComponent(shoes) &&
+                         itemToggleQuery.TryGetComponent(shoes, out var toggle) &&
+                         toggle.Activated)
+                {
+                    hasMagboots = true;
+                }
+            }
+
+            if (hasMagboots)
+                continue;
+
+            // Apply knockdown to unbuckled entities
+            _stuns.TryKnockdown(uid, knockdownTime, true);
+        }
     }
 
     private void ProcessTile(EntityUid uid, MapGridComponent grid, Vector2i tile, float energy, Vector2 dir)
