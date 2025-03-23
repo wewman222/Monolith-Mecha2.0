3d3ac4ce702dca20ee29ea25f5452f671fd04835
Ark <189933909+ark1368@users.noreply.github.com>
2025-03-22 21:32:49 -0700

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,8 +15,8 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.frontierstation14.com/"
-FORK_ID = "Frontier"
+ROBUST_CDN_URL = "https://cdn.project-monolith.xyz/"
+FORK_ID = "monolith"
 
 def main():
     parser = argparse.ArgumentParser()
