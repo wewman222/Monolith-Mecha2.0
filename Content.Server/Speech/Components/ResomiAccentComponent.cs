41103f7d3e3d13e50b8c23d847d5ac496a479981
Redrover1760 <39284090+Redrover1760@users.noreply.github.com>
2025-06-14 06:47:49 -0400

    resomi silly bawk (#816)
    * bawk
    
    * accent
    
    * sillier

---
+++ Content.Server/Speech/Components/ResomiAccentComponent.cs
@@ -0,0 +1,19 @@
+﻿using Content.Server.Speech.EntitySystems;
+
+namespace Content.Server.Speech.Components;
+
+/// <summary>
+///     Silly BAWK!
+/// </summary>
+[RegisterComponent]
+[Access(typeof(ResomiAccentSystem))]
+public sealed partial class ResomiAccentComponent : Component
+{
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("bawkChance")]
+    public float BawkChance = 0.01f;
+
+
+
+
+}
---
+++ Content.Server/Speech/EntitySystems/ResomiAccentSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+using Content.Server.Speech.Components;
+using Robust.Shared.Random;
+
+namespace Content.Server.Speech.EntitySystems;
+
+public sealed class ResomiAccentSystem : EntitySystem
+{
+
+    [Dependency] private readonly IRobustRandom _random = default!;
+
+    private static readonly Regex RegexLowerSilly = new Regex("silly");
+    private static readonly Regex RegexFirstCapSilly = new Regex("Silly");
+    private static readonly Regex RegexUpperSilly = new Regex("SILLY");
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<ResomiAccentComponent, AccentGetEvent>(OnAccent);
+    }
+
+    private void OnAccent(EntityUid uid, ResomiAccentComponent component, AccentGetEvent args)
+    {
+        var message = args.Message;
+
+        if (_random.Prob(component.BawkChance))
+        {
+
+            // bawk
+            message = RegexLowerSilly.Replace(message, "silly... bawk");
+            // Bawk
+            message = RegexFirstCapSilly.Replace(message, "Silly... Bawk");
+            // BAWK
+            message = RegexUpperSilly.Replace(message, "SILLY... BAWK");
+        }
+
+        args.Message = message;
+    }
+}
