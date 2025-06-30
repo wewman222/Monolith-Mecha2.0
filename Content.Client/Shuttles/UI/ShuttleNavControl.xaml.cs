2251b704702d67effde259e93678ca991bc95d1a
Blu <79374236+BlueHNT@users.noreply.github.com>
2025-06-30 06:01:41 +0200

    Tries to Fix some Test Fails (#978)
    * batch 1 fixes of ubuntu
    
    * chore: automatically update REUSE headers
    
    * removes shoddy C# work
    
    * Revert "batch 1 fixes of ubuntu"
    
    This reverts commit 8cbf2f0d6ca3b61219d4b43fd200340062acc5d8.
    
    * refactor: clean up SPDX copyright headers and update parent references in security and 45_magnum recipes
    
    * chore: automatically update REUSE headers
    
    ---------
    
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>

--- Content.Client/Shuttles/UI/ShuttleNavControl.xaml.cs
+++ Content.Client/Shuttles/UI/ShuttleNavControl.xaml.cs
@@ -1,3 +1,22 @@
+// SPDX-FileCopyrightText: 2024 ArchRBX
+// SPDX-FileCopyrightText: 2024 ErhardSteinhauer
+// SPDX-FileCopyrightText: 2024 Nemanja
+// SPDX-FileCopyrightText: 2024 Wiebe Geertsma
+// SPDX-FileCopyrightText: 2024 eoineoineoin
+// SPDX-FileCopyrightText: 2024 exincore
+// SPDX-FileCopyrightText: 2024 leonarudo
+// SPDX-FileCopyrightText: 2024 metalgearsloth
+// SPDX-FileCopyrightText: 2024 neuPanda
+// SPDX-FileCopyrightText: 2025 Alex Parrill
+// SPDX-FileCopyrightText: 2025 Ark
+// SPDX-FileCopyrightText: 2025 BlueHNT
+// SPDX-FileCopyrightText: 2025 GreaseMonk
+// SPDX-FileCopyrightText: 2025 LukeZurg22
+// SPDX-FileCopyrightText: 2025 RikuTheKiller
+// SPDX-FileCopyrightText: 2025 Whatstone
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Numerics;
 using Content.Client.Station; // Frontier
 using Content.Shared.Shuttles.BUIStates;
